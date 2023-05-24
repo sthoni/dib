5a26e88799bf2f75991651048834cef44a1c4727
Sebastian Thon <sthon@hotmail.de>
2023-05-25 00:36:02 +0200

    CSS reset und font-size

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Digitalisierung an Schulen.";
+export const SITE_DESCRIPTION =
+  "Tipps und Hinweise zur Digitalisierung an Schulen: Sowohl im Unterricht als auch in der Vorbereitung und Verwaltung.";
